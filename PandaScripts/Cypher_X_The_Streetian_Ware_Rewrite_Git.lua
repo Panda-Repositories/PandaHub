ed4d2eab8c4dbcd594d6fed3370bfefce97fda9d
SkieHacker <40538401+SkieAdmin@users.noreply.github.com>
2021-12-18 21:41:32 -0800

    Not mine lmfao

---
+++ PandaScripts/Cypher_X_The_Streetian_Ware_Rewrite_Git.lua
@@ -0,0 +1,22 @@
+--[[
+_________               .__                   ____  ___
+\_   ___ \___.__.______ |  |__   ___________  \   \/  /
+/    \  \<   |  |\____ \|  |  \_/ __ \_  __ \  \     / 
+\     \___\___  ||  |_> >   Y  \  ___/|  | \/  /     \ 
+ \______  / ____||   __/|___|  /\___  >__|    /___/\  \
+        \/\/     |__|        \/     \/              \_/
+        
+Da-Hoodian Ware Aimlock for The Streets
+-------------------------------------------------------
+Made by Enviie and help with Hunter
+-------------------------------------------------------
+Supports Multiple Games
+Gamelink:
+https://www.roblox.com/games/455366377/The-Streets
+https://www.roblox.com/games/4669040/The-Prison
+https://www.roblox.com/games/8151566928/The-Streets-But-Not-Garbage
+https://www.roblox.com/games/8156411551/Streets-remake
+https://www.roblox.com/games/8046624339/The-Streets-Remake
+]]
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Enviie/Cypher-X-The-Streetian-Ware-Rewrite/main/Cypher%20X%20The%20Streetian%20Ware%20Rewrite%20Main.lua"))("Enviie Soo Hot!")
