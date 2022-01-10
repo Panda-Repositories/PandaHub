ac41f5dcc8b829499b7df0dc44f33d2792233e1e
SkieHacker <40538401+SkieAdmin@users.noreply.github.com>
2022-01-10 19:47:19 +0800

    Add files via upload

---
+++ Cypher_X_Jailbird-Ware_Loadstring.lua
@@ -0,0 +1,19 @@
+--[[
+_________               .__                   ____  ___
+\_   ___ \___.__.______ |  |__   ___________  \   \/  /
+/    \  \<   |  |\____ \|  |  \_/ __ \_  __ \  \     / 
+\     \___\___  ||  |_> >   Y  \  ___/|  | \/  /     \ 
+ \______  / ____||   __/|___|  /\___  >__|    /___/\  \
+        \/\/     |__|        \/     \/              \_/
+        
+Cheats for Jailbird
+-------------------------------------------------------
+Made by Enviie
+-------------------------------------------------------
+Changelogs: Later gonna add it lazy right now
+-------------------------------------------------------
+Gamelink:
+https://www.roblox.com/games/6055959032/Jailbird-Beta-14-0
+]]
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Enviie/Cypher-X-Jailbird-Ware/main/Jailbird-Ware%20Main.lua"))("Very Cool Swag")
